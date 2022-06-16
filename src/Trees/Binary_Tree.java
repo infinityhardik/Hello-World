cec9b79e43c40f91c5dfe5a466d9c658ae254eb9
Hardik Bhaavani <bhaavanihardik@outlook.com>
2022-06-16 09:48:45 +0530

    Added Git Ignore

--- src/Trees/Binary_Tree.java
+++ src/Trees/Binary_Tree.java
@@ -34,13 +34,13 @@ public class Binary_Tree {
         if(root == null){
             return;
         }
- 
+        
         postOrderDFS(root.left);
         postOrderDFS(root.right);
         System.out.print(root.data +" ");
     }
-
-    // Performs Left to Right Breadth First Search
+    
+    // Performs Left to Right Breadth First Search (Iterative)
     public void BFS(Node root){
         if(root != null){
             Queue<Node> q = new LinkedList<>();
@@ -60,16 +60,53 @@ public class Binary_Tree {
             System.out.println("Root is Null");
         }
     }
- 
+    
+    // Method to calculate height of a tree
+    public int height(Node root) {
+        if (root == null)
+        return 0;
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+        
+        if (leftHeight > rightHeight)
+        return leftHeight + 1;
+        else
+        return rightHeight + 1;
+    }
+    
+    // Method to traverse the elements of a tree using BFS (level-order traversal) in recursive way
+    public void recursiveBFS(Node root) {
+        int h = height(root);
+        for (int i = 0; i < h; i++)
+        printNodesAtLevel(root, i, 0);
+
+        /* 
+        for (int i = 1; i <= h; i++)
+        printNodesAtLevel(root, i, 1); 
+        */
+    }
+
+    // Method to print nodes at the given level
+    public void printNodesAtLevel (Node root, int level, int currentLevel) {
+        if (root == null)
+        return;
+        if (level == currentLevel)
+        System.out.print(root.data + " ");
+        else {
+            printNodesAtLevel(root.left, level, currentLevel + 1);
+            printNodesAtLevel(root.right, level, currentLevel + 1);
+        }
+    }
+    
     public static void main(String[] args) {
         Binary_Tree t1 = new Binary_Tree();
         Node root =  t1.new Node("A");
- 
+        
         root.left = t1.new Node("B");
         root.right = t1.new Node("C");
         root.left.right = t1.new Node("D");
         root.right.left = t1.new Node("E");
-
+        
         System.out.println("In-Order DFS Traversal");
         t1.inOrderDFS(root);
         System.out.println();
@@ -79,7 +116,10 @@ public class Binary_Tree {
         System.out.println("Post-Order DFS Traversal");
         t1.postOrderDFS(root);
         System.out.println();
-        System.out.println("BFS Traversal");
+        System.out.println("Iterative BFS Traversal");
         t1.BFS(root);
+        System.out.println("Recursive BFS Traversal");
+        t1.recursiveBFS(root);
+        System.out.println();
     }
 }
