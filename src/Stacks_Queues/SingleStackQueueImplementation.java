477cb3f5ec835195404b84bb2c25727d6b9ccace
Hardik Bhaavani <bhaavanihardik@outlook.com>
2022-06-17 10:18:52 +0530

    Single Stack Queue Implementation

---
+++ src/Stacks_Queues/SingleStackQueueImplementation.java
@@ -0,0 +1,72 @@
+/* 
+Implements a Queue using a Single Stack , i.e., Stack acts like a Queue
+
+Write a program to implement the basic methods of 'queue', i.e. enqueue( ) and 
+dequeue( ) using a single stack. 
+
+Constraint:  The time complexity of your code in the worst case should be O(n).
+
+
+Input:
+6
+1 2 3 4 5 6
+
+Output: 
+1
+2
+3
+*/
+package Stacks_Queues;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+class ImplementQueue{
+    
+    Stack<Integer> stack = new Stack<Integer>();
+    
+    //The basic push and pop method of a stack are used to create enqueue and dequeue function of a queue
+    
+    public void enQueue(int newData){
+        // Normal Stack Push Operation
+        stack.push(newData);
+    }
+    
+    
+    // Function to deQueue an item from queue 
+    public void deQueue(){
+        
+        // Using Recursion, i.e., Program Stack to reach the First Element of the Stack in order to achieve FIFO.  
+        
+        if(stack.isEmpty()){
+            System.out.println("No Elements Left");
+            return;
+        }
+        
+        if(stack.size()==1){
+            System.out.println(stack.pop());
+            return;
+        }
+        
+        int elem = stack.pop();
+        deQueue();
+        stack.push(elem);
+    } 
+}
+
+public class SingleStackQueueImplementation { 
+    public static void main(String[] args) {
+        
+        Scanner in = new Scanner(System.in);
+        ImplementQueue obj = new ImplementQueue();
+        int n=in.nextInt();
+        for(int i=0;i<n;i++) {
+            obj.enQueue(in.nextInt());   
+            
+        }  
+        obj.deQueue();
+        obj.deQueue();
+        obj.deQueue();
+        in.close();
+    }
+}
