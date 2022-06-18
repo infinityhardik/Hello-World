b18eae6054c0530a2f2c5712b503ace566ab94e8
Hardik Bhaavani <bhaavanihardik@outlook.com>
2022-06-18 09:19:27 +0530

    Added Search to BST

--- src/Trees/Binary_Search_Tree.java
+++ src/Trees/Binary_Search_Tree.java
@@ -1,5 +1,7 @@
 package Trees;
 
+import java.util.Scanner;
+
 public class Binary_Search_Tree {
     static class TreeNode {
         TreeNode left, right; 
@@ -13,7 +15,7 @@ public class Binary_Search_Tree {
         if(root == null) {
             return new TreeNode(data);
         }
-
+        
         if(root.data > data) {
             root.left = recInsert(root.left, data);
         } else {
@@ -32,7 +34,7 @@ public class Binary_Search_Tree {
         System.out.print(root.data+" ");
         inorder(root.right);
     }
-
+    
     static TreeNode insertIter(TreeNode root, int data) {
         if(root == null) {
             return new TreeNode(data);
@@ -60,13 +62,37 @@ public class Binary_Search_Tree {
         
         return root;
     }
+    
+    // Method to search for a node with given key in a BST
+    static boolean search(TreeNode node, int key) {
+        if (node == null)
+        return false;
+        
+        if (key == node.data)
+        return true;
+        
+        if (key < node.data)
+        return search(node.left, key);
+        else
+        return search(node.right, key);
+    }
     public static void main(String args[]) {
-        int arr[] = {10, 30, 50, 5, 15, 3};
+        // int arr[] = {10, 30, 50, 5, 15, 3};
+        // root = recInsert(root, arr[i]);
+        // root = insertIter(root, arr[i]);
+        
+        Scanner in = new Scanner(System.in);
         TreeNode root = null;
-        for(int i =0; i < arr.length; i++) {
-            //root = recInsert(root, arr[i]);
-            root = insertIter(root, arr[i]);
+        System.out.println("Enter No. of Elements to be inserted in BST :");
+        int n = in.nextInt();
+        for(int i =0; i < n; i++) {
+            root = recInsert(root, in.nextInt());
         }
         inorder(root);
+        System.out.println();
+        System.out.println("Enter a Search Key : ");
+        int key = in.nextInt();
+        System.out.println(search(root, key));
+        in.close();
     }
 }
