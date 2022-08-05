68073e8aa55658e9ab3e1560d14364de470108de
Hardik Bhaavani <bhaavanihardik@outlook.com>
2022-08-05 10:24:25 +0530

    Modified

--- JavaScript/Interactive Project Management App/JavaScript-Session 3 Segment 2/scripts/home.js
+++ JavaScript/Interactive Project Management App/JavaScript-Session 3 Segment 2/scripts/home.js
@@ -6,3 +6,11 @@ function toggleMenu() {
         toggleVariable.style.display = 'none'
     }
 }
+
+/* 
+While changing the diaplay of an element in JavaScript, one of the ways to undo the effect of  
+style.display = 'none' is by setting it to style.display = '' 
+This way, the display declaration from the style attribute is removed altogether 
+and is reverted to the actual value of the display property which is declared in the style sheet. 
+If it is not defined anywhere in the style sheet, it is reverted to the default value of the browser. 
+*/
--- JavaScript/Interactive Project Management App/JavaScript-Session 3 Segment 3/scripts/home.js
+++ JavaScript/Interactive Project Management App/JavaScript-Session 3 Segment 3/scripts/home.js
@@ -7,10 +7,15 @@ var projectListObject = [
     }
 ];
 
+// projectListObject.forEach(function(value, index){console.log(value);});
+// To check the Output of Value.
+/* The forEach loop is used to iterate over an array and calls the defined function once for each array element. 
+The forEach loop takes a function and passes two parameters; 
+the index represents the number of iterations and the value representing each object in the array. */
+
 showProject();
 
 function showProject(){
-   
 
     projectListObject.forEach(function(value,index){
         var template = '<div class ="project-card">'+
