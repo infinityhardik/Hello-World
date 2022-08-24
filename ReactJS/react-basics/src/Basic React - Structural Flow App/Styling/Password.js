39cd8d90aac8e8e3c70c032d0ed10c5c65a09072
Hardik Bhaavani <bhaavanihardik@outlook.com>
2022-08-24 21:18:45 +0530

    Styling Example

---
+++ ReactJS/react-basics/src/Basic React - Structural Flow App/Styling/Password.js
@@ -0,0 +1,16 @@
+// 'Password' as a functional component to render the password input element
+// DONE 3: Styled Password component using styles written in external stylesheet (external styling)
+import React from 'react';
+
+const Password = function (props) {
+    return (
+        <div className='password-container'>
+            <input id='password' type='password' placeholder='Type your password here' />
+        </div>
+    );
+}
+
+export default Password;
+
+// <input style={{fontSize:"15px"}} id='password' type='password' placeholder='Type your password here' />
+// This is Inline Styling.
